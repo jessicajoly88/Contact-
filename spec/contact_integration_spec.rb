ad6259ba84fbe47ddf954903dba07318def348f6
Alexa Zeazas & Jessica Joly <student@epicodus.com>
2015-08-13 15:53:09 -0700

    Add contact path integration spec

--- spec/contact_integration_spec.rb
+++ spec/contact_integration_spec.rb
@@ -14,3 +14,21 @@ describe('contacts organizer path', {:type => :feature}) do
     expect(page).to have_content("Success!")
   end
 end
+
+describe('contact path', {:type => :feature}) do
+  it('processes user entry and adds it to the contact') do
+    visit('/contact/:id')
+    fill_in('street_address', :with => '123 Test St.')
+    fill_in('city', :with => 'Portland')
+    fill_in('state', :with => 'OR')
+    fill_in('zip', :with => '97204')
+    fill_in('mail_type', :with => 'Work')
+    fill_in('area_code', :with => '(503)')
+    fill_in('number', :with => '555-1452')
+    fill_in('phone_type', :with => 'Work')
+    fill_in('email', :with => 'testadd@gmail.com')
+    fill_in('email_type', :with => 'Work'
+    click_button('Add')
+    expect(page).to have_content("Success!")
+  end
+end
