b8a4476e8e3e19668afcc2570661abb9b10ebacd
Alexa Zeazas & Jessica Joly <student@epicodus.com>
2015-08-13 12:15:32 -0700

    Add type method to Mail_address class.

--- spec/mail_address_spec.rb
+++ spec/mail_address_spec.rb
@@ -31,10 +31,10 @@ describe(Mail_address) do
     end
   end
 
-  describe('#street_address') do
-    it("returns the street address") do
+  describe('#type') do
+    it("returns the type") do
       test_mail = Mail_address.new({:street_address => "123 Test St.", :city => "Portland", :state => "OR", :zip => "97204", :type => "Work"})
-      expect(test_mail.street_address()).to(eq("123 Test St."))
+      expect(test_mail.type()).to(eq("Work"))
     end
   end
 
