5b6ed0a29b97b9c99cc06f97a8526f291ca1ce85
Alexa Zeazas & Jessica Joly <student@epicodus.com>
2015-08-13 12:13:09 -0700

    Add city method to Mail_address class.

--- spec/mail_address_spec.rb
+++ spec/mail_address_spec.rb
@@ -10,4 +10,32 @@ describe(Mail_address) do
     end
   end
 
+  describe('#city') do
+    it("returns the city") do
+      test_mail = Mail_address.new({:street_address => "123 Test St.", :city => "Portland", :state => "OR", :zip => "97204", :type => "Work"})
+      expect(test_mail.city()).to(eq("Portland"))
+    end
+  end
+
+  describe('#street_address') do
+    it("returns the street address") do
+      test_mail = Mail_address.new({:street_address => "123 Test St.", :city => "Portland", :state => "OR", :zip => "97204", :type => "Work"})
+      expect(test_mail.street_address()).to(eq("123 Test St."))
+    end
+  end
+
+  describe('#street_address') do
+    it("returns the street address") do
+      test_mail = Mail_address.new({:street_address => "123 Test St.", :city => "Portland", :state => "OR", :zip => "97204", :type => "Work"})
+      expect(test_mail.street_address()).to(eq("123 Test St."))
+    end
+  end
+
+  describe('#street_address') do
+    it("returns the street address") do
+      test_mail = Mail_address.new({:street_address => "123 Test St.", :city => "Portland", :state => "OR", :zip => "97204", :type => "Work"})
+      expect(test_mail.street_address()).to(eq("123 Test St."))
+    end
+  end
+
 end
