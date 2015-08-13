fd74f51fcc5dd9212d1ba5bb523407de9c911131
Alexa Zeazas & Jessica Joly <student@epicodus.com>
2015-08-13 16:59:53 -0700

    Add (unsuccessful) contact path route.

--- app.rb
+++ app.rb
@@ -16,3 +16,18 @@ post("/") do
   contact.save()
   erb(:success)
 end
+
+get("/contact/:id") do
+  @contact = Contact.find(params.fetch('id').to_i())
+  erb(:contact)
+end
+
+post("/contact/:id") do
+  email = Email.new({:address => params.fetch("email"),:type => 'email_type'})
+  email.save()
+  mail = Mail_address.new({:street_address => params.fetch('street_address'), :city => params.fetch('city'), :state => params.fetch('state'), :zip => params.fetch('zip'), :type => params.fetch('mail_type')})
+  mail.save()
+  phone = Phone_number.new({:area_code => params.fetch('area_code'), :number => params.fetch('number'),:type => params.fetch('phone_type')})
+  phone.save()
+  erb(:success)
+end
