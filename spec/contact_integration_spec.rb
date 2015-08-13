c8754a5e3e2efdac36e21941712b1b3be013ff50
Alexa Zeazas & Jessica Joly <student@epicodus.com>
2015-08-13 15:09:02 -0700

    Add root path route integration spec.

---
+++ spec/contact_integration_spec.rb
@@ -0,0 +1,23 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('contacts organizer path', {:type => :feature}) do
+  it('processes the user entry and adds it to the contacts list') do
+    visit('/')
+    fill_in('first_name', :with => 'Luigi')
+    fill_in('last_name', :with => 'Bro')
+    fill_in('job_title', :with => 'plumber')
+    fill_in('company', :with => 'Super Marios Bros.')
+    click_button('Add')
+    expect(page).to have_content("Success!")
+  end
+end
+
+# describe('the add contact path', {:type => :feature}) do
+#   it('processes the user entry and adds a new contact') do
+#     visit('')
+#
+#   end
+# end
